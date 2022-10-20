1e8444183b80590dc24e0e4f5f5a49de24552016
Luismanuel1996 <luismanuel1996@live.com>
2022-10-20 15:09:07 -0400

    done

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-console.log("EXERCISE 1:\n==========\n")
 
 let a = 20 ;
 let b = 4 ;
@@ -11,9 +10,7 @@ let add = ( a + b ) ;
 let minus = ( a - b ) ;
 let multiply = ( a * b ) ;
 let dividing = ( a / b ) ;
-
-
-console.log("EXERCISE 2:\n==========\n");
+console.log(add, minus, multiply, dividing)
 
 let num = 11 ;
 let str = "11" ;
@@ -22,16 +19,12 @@ let isPresent = true;
 let firstName = "Frodo" ;
 let lastName = "Baggins" ;
 
-
-- What is the value of: num + str? 1111
-- What is the value of: num + str2? 11eleven
-- What is the value of: num + isPresent? 12
-- What is the value of: firstName + num? Frodo11
-- What is the value of: isPresent + str? true11
-- What is the value of: firstName + lastName? FrodoBaggins
-
-
-console.log("EXERCISE 3:\n==========\n");
+What is the value of: num + str? 1111
+What is the value of: num + str2? 11eleven
+What is the value of: num + isPresent? 12
+What is the value of: firstName + num? Frodo11
+What is the value of: isPresent + str? true11
+What is the value of: firstName + lastName? FrodoBaggins
 
 let val = 5;
 let str3 = "5";
@@ -42,7 +35,7 @@ What is the value of: val == str3? true
 - What is the value of: val === str3? false
 - What is the value of: !isAwake? true
 - What is the value of: ("eleven" == str4 && val >= str3)? false
-- What is the value of: (!isAwake || isAwake)? 
+- What is the value of: (!isAwake || isAwake)? true
 - What is the value of: 0 == false? true
 - What is the value of: 0 === false? false
 - What is the value of: 0 != false? false
